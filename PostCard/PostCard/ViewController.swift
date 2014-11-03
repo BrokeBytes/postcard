0cc54aa9bb951c52b83eb4ab5ad92e695acf057c
James Dykstra <brokebytes@Jamess-MacBook-Pro.local>
2014-11-03 15:58:40 -0800

    Initial Commit of Xcode Project for PostCard

---
+++ PostCard/PostCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostCard
+//
+//  Created by James Dykstra on 11/3/14.
+//  Copyright (c) 2014 Broke Bytes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
